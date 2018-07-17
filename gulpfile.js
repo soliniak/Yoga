028697c4ae45f70250078433bf54e300fea7aa11
Rafi <soliniak@gmail.com>
2018-07-17 23:00:18 +0200

    First commit webdev challenge no 16

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('watch', ['sass', 'browserSync'], function () {
+  gulp.watch('./sass/**/*.sass', ['sass']);
+  gulp.watch('sass/**/*.sass', browserSync.reload);
+  gulp.watch('*.html', browserSync.reload);
+  gulp.watch('*.js', browserSync.reload);
+});
+
+
+//Watch task
+// gulp.task('watch', ['browserSync', 'styles', 'prefix'], function() {
+//     gulp.watch('sass/**/*.sass', ['prefix']);
+//     gulp.watch('sass/**/*.sass',['styles']);
+// 	   gulp.watch('sass/**/*.sass', browserSync.reload);
+//     gulp.watch('*.html', browserSync.reload);
+//     gulp.watch('*.js', browserSync.reload);
+// });
